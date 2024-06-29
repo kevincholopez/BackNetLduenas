03dee176b959985956ce1d0224f5bcf0c22b5354
kevincholopez <ingkevinlopez@outlook.com>
2024-06-28 22:35:49 -0500

    subirp royecto net

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DemoCS1.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public string Version()
+        {
+            return "1.0.0.0";
+        }
+    }
+}
